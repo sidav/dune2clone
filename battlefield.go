8ece8d2dd8f4e04c77339e9ab52968de7f800134
sidav <sidav94@gmail.com>
2022-07-23 13:41:21 +0300

    WIP on pathfinding.

--- battlefield.go
+++ battlefield.go
@@ -1,23 +1,14 @@
 package main
 
+import "dune2clone/astar"
+
 type battlefield struct {
 	tiles     [][]tile
 	buildings []*building
 	units     []*unit
 
+	pathfinder  *astar.AStarPathfinder
 	currentTick int
-}
-
-func (b *battlefield) getActorAtTileCoordinates(x, y int) actor {
-	for i := range b.units {
-		tx, ty := trueCoordsToTileCoords(b.units[i].centerX, b.units[i].centerY)
-		// debugWritef("req: %d,%d; act: %f, %f -> %d, %d \n", x, y, b.units[i].centerX, b.units[i].centerY, tx, ty)
-		if tx == x && ty == y {
-			// debugWrite("got")
-			return b.units[i]
-		}
-	}
-	return nil
 }
 
 func (b *battlefield) create(w, h int) {
@@ -28,6 +19,15 @@ func (b *battlefield) create(w, h int) {
 			b.tiles[i][j].code = TILE_SAND
 		}
 	}
+	b.pathfinder = &astar.AStarPathfinder{
+		DiagonalMoveAllowed:       false,
+		ForceGetPath:              true,
+		ForceIncludeFinish:        false,
+		AutoAdjustDefaultMaxSteps: false,
+		MapWidth:                  len(b.tiles),
+		MapHeight:                 len(b.tiles[0]),
+	}
+
 	b.buildings = append(b.buildings, &building{
 		topLeftX: 1,
 		topLeftY: 1,
@@ -44,4 +44,30 @@ func (b *battlefield) create(w, h int) {
 		centerX: 5.5,
 		centerY: 5.5,
 	})
+}
+
+func (b *battlefield) getActorAtTileCoordinates(x, y int) actor {
+	for i := range b.units {
+		tx, ty := trueCoordsToTileCoords(b.units[i].centerX, b.units[i].centerY)
+		// debugWritef("req: %d,%d; act: %f, %f -> %d, %d \n", x, y, b.units[i].centerX, b.units[i].centerY, tx, ty)
+		if tx == x && ty == y {
+			// debugWrite("got")
+			return b.units[i]
+		}
+	}
+	return nil
+}
+
+func (b *battlefield) findPathForUnitTo(u *unit, tileX, tileY int) *astar.Cell {
+	utx, uty := trueCoordsToTileCoords(u.centerX, u.centerY)
+	return b.pathfinder.FindPath(
+		func(x, y int) int {
+			act := b.getActorAtTileCoordinates(x, y)
+			if act != nil && act != u {
+				return -1
+			}
+			return 1
+		},
+		utx, uty, tileX, tileY,
+	)
 }
--- battlefield_execute_order.go
+++ battlefield_execute_order.go
@@ -2,7 +2,7 @@ package main
 
 func (b *battlefield) executeOrderForUnit(u *unit) {
 	if u.currentAction.code != ACTION_WAIT {
-		return // execute the order only after finishing the action
+		// return // execute the order only after finishing the action
 	}
 	if u.currentOrder.code == ORDER_NONE {
 		return // TODO: look around for targets etc
@@ -18,19 +18,15 @@ func (b *battlefield) executeMoveOrder(u *unit) {
 	// x, y := u.centerX, u.centerY
 	utx, uty := trueCoordsToTileCoords(u.centerX, u.centerY)
 	// todo: pathfinding
-	otx, oty := u.currentOrder.targetTileX, u.currentOrder.targetTileY
-	u.currentAction.code = ACTION_MOVE
-	u.currentAction.targetTileX = utx
-	u.currentAction.targetTileY = uty
-	// debugWritef("Tick %d: action assigned\n", b.currentTick)
+	orderTileX, orderTileY := u.currentOrder.targetTileX, u.currentOrder.targetTileY
+	path := b.findPathForUnitTo(u, orderTileX, orderTileY)
+	vx, vy := path.GetNextStepVector()
 
-	if abs(otx-utx) > abs(oty-uty) {
-		u.currentAction.targetTileX = utx + sign(otx-utx)
-	} else if oty != uty {
-		u.currentAction.targetTileY = uty + sign(oty-uty)
-	}
-	// debugWritef("Order: %+v, action: %+v\n", u.currentOrder, u.currentAction)
-	if utx == otx && uty == oty {
+	u.currentAction.code = ACTION_MOVE
+	u.currentAction.targetTileX = utx + vx
+	u.currentAction.targetTileY = uty + vy
+
+	if utx == orderTileX && uty == orderTileY {
 		u.currentOrder.code = ORDER_NONE
 	}
 }
