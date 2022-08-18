9a296a0ffbdcebf5e08fbebe8639503eddad4443
sidav <sidav94@gmail.com>
2022-08-19 02:42:33 +0300

    WIP.

--- battlefield_create.go
+++ battlefield_create.go
@@ -5,32 +5,6 @@ import (
 	"dune2clone/map_generator"
 )
 
-//func (b *battlefield) initEmpty(w, h int) {
-//	b.tiles = make([][]tile, w)
-//	for i := range b.tiles {
-//		b.tiles[i] = make([]tile, h)
-//		for j := range b.tiles[i] {
-//			b.tiles[i][j].code = TILE_SAND
-//		}
-//	}
-//	b.pathfinder = &astar.AStarPathfinder{
-//		DiagonalMoveAllowed:       true,
-//		ForceGetPath:              true,
-//		ForceIncludeFinish:        false,
-//		AutoAdjustDefaultMaxSteps: false,
-//		MapWidth:                  len(b.tiles),
-//		MapHeight:                 len(b.tiles[0]),
-//	}
-//	// place some resources
-//	for x := MAP_W/5; x < 4*MAP_W/5; x++ {
-//		for y := 0; y < MAP_H; y++ {
-//			b.tiles[x][y].resourcesAmount = rnd.RandInRange(250, 500)
-//		}
-//	}
-//	b.placeInitialStuff()
-//	b.finalizeTileVariants()
-//}
-
 func (b *battlefield) initFromRandomMap(rm *map_generator.GameMap) {
 	b.tiles = make([][]tile, len(rm.Tiles))
 	for i := range b.tiles {
@@ -77,8 +51,8 @@ func (b *battlefield) initFromRandomMap(rm *map_generator.GameMap) {
 func (b *battlefield) finalizeTileVariants() {
 	for i := range b.tiles {
 		for j := range b.tiles[i] {
-			debugWritef("CODE: %d ", b.tiles[i][j].code)
-			debugWritef("CONTENTS: %+v\n", sTableTiles[b.tiles[i][j].code])
+			//debugWritef("CODE: %d ", b.tiles[i][j].code)
+			//debugWritef("CONTENTS: %+v\n", sTableTiles[b.tiles[i][j].code])
 			b.tiles[i][j].spriteVariantIndex = rnd.Rand(len(sTableTiles[b.tiles[i][j].code].spriteCodes))
 		}
 	}
@@ -86,17 +60,18 @@ func (b *battlefield) finalizeTileVariants() {
 
 func (b *battlefield) placeInitialStuff(startPoints [][2]int) {
 	for spNumber := range startPoints {
-		b.factions = append(b.factions, createFaction(spNumber, 0, 100000, 2))
+		b.factions = append(b.factions, createFaction(spNumber, 0, 10000, 2))
 		b.factions[spNumber].resetVisibilityMaps(len(b.tiles), len(b.tiles[0]))
 		b.factions[spNumber].exploreAround(startPoints[spNumber][0], startPoints[spNumber][1], 2, 2, 3)
 		b.addActor(createBuilding(BLD_BASE, startPoints[spNumber][0], startPoints[spNumber][1], b.factions[spNumber]))
 		// b.addActor(createUnit(UNT_HARVESTER, startPoints[spNumber][0]-1, startPoints[spNumber][1]-1, b.factions[spNumber]))
 	}
-	unt := createUnit(AIR_TRANSPORT, startPoints[0][0], startPoints[0][1], b.factions[0])
+	b.factions[0].resourcesMultiplier = 1 // for player
+	b.ais = append(b.ais, createAi(b.factions[0], "Player-side"))
+	// b.ais = append(b.ais, createAi(b.factions[1], "Enemy"))
+
+	// unt := createUnit(AIR_TRANSPORT, startPoints[0][0], startPoints[0][1], b.factions[0])
 	// unt.currentAction.code = ACTION_AIR_APPROACH_LAND_TILE
 	// unt.currentAction.setTargetTileCoords(10, 10)
-	b.addActor(unt)
-	b.factions[0].resourcesMultiplier = 1 // for player
-	//b.ais = append(b.ais, createAi(b.factions[0], "Player-side"))
-	//b.ais = append(b.ais, createAi(b.factions[1], "Enemy"))
+	// b.addActor(unt)
 }
