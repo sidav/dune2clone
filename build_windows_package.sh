a9c95e7f02e3dd11b6e387c20a55b1deb1a69e0b
sidav <sidav94@gmail.com>
2023-02-06 19:27:56 +0300

    Adds the build script.

---
+++ build_windows_package.sh
@@ -0,0 +1,14 @@
+DATE=$(date +%d_%m_%y)
+EXE_NAME=sidavRTS_${DATE}.exe
+ZIP_NAME=sidavRTS${DATE}.zip
+
+echo "Building ${EXE_NAME}..."
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags="-s -w" -o ${EXE_NAME} *.go
+
+echo "Packing the data to ${ZIP_NAME}..."
+zip -qr ${ZIP_NAME} ${EXE_NAME} resources
+
+echo "Removing residual ${EXE_NAME}..."
+rm ${EXE_NAME}
+
+echo "Build completed."
