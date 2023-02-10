a4694d2839d4e2f81d44c1d1b9ae56bb61343eb0
sidav <sidav94@gmail.com>
2023-02-10 13:53:30 +0300

    Hides console for Windows builds.

--- build_windows_package.sh
+++ build_windows_package.sh
@@ -3,7 +3,7 @@ EXE_NAME=sidavRTS_${DATE}.exe
 ZIP_NAME=sidavRTS_${DATE}.zip
 
 echo "Building ${EXE_NAME}..."
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags="-s -w" -o ${EXE_NAME} *.go
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags="-s -w -H=windowsgui" -o ${EXE_NAME} *.go
 
 echo "Packing the data to ${ZIP_NAME}..."
 zip -qr ${ZIP_NAME} ${EXE_NAME} resources
